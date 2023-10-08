f5a7cbad140acf5584a7d13c60c9533396ddc808
reinareign <chisohmenem@gmail.com>
2023-10-08 02:27:34 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Write a C program that prints a line, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
