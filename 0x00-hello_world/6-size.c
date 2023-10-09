208fac61626358f48fa9e19829d7fa4bf6df482a
reinareign <chisohmenem@gmail.com>
2023-10-09 01:40:37 -0700

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
-/**
- * main - Entry Point
+/*
+ * main: Entry Point
+ *
+ * Description: C that prints the size of various types on the computer
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("size of a char: %d byte(s)\n", sizeof(char));
-	printf("size of an int: %d byte(s)\n", sizeof(int));
-	printf("size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("size of a float: %d byte(s)\n", sizeof(float));
+	printf("size of char: %lu byte(s)\n", sizeof(char));
+	printf("size of int: %lu byte(s)\n", sizeof(int));
+	printf("size of float: %lu byte(s)\n", sizeof(float));
+	printf("size of double: %lu byte(s)\n", sizeof(double));
+	printf("size of long: %lu byte(s)\n", sizeof(long));
+	printf("size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("size of short: %lu byte(s)\n", sizeof(short));
+	printf("size of unsigned int: %lu byte(s)\n", sizeof(unsigned int));
+	printf("size of char pointer: %lu byte(s)\n", sizeof(char *));
+	printf("size of int pointer: %lu byte(s)\n", sizeof(int *));
+	printf("size of float pointer: %lu byte(s)\n", sizeof(float *));
+	printf("size of double pointer: %lu byte(s)\n", sizeof(double *));
 	return (0);
 }
