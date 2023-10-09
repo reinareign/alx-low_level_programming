de947cad14e09acefe714a7b29f37c1efecf2951
reinareign <chisohmenem@gmail.com>
2023-10-09 01:45:16 -0700

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,14 +11,7 @@ int main(void)
 	printf("size of char: %lu byte(s)\n", sizeof(char));
 	printf("size of int: %lu byte(s)\n", sizeof(int));
 	printf("size of float: %lu byte(s)\n", sizeof(float));
-	printf("size of double: %lu byte(s)\n", sizeof(double));
 	printf("size of long: %lu byte(s)\n", sizeof(long));
 	printf("size of long long: %lu byte(s)\n", sizeof(long long));
-	printf("size of short: %lu byte(s)\n", sizeof(short));
-	printf("size of unsigned int: %lu byte(s)\n", sizeof(unsigned int));
-	printf("size of char pointer: %lu byte(s)\n", sizeof(char *));
-	printf("size of int pointer: %lu byte(s)\n", sizeof(int *));
-	printf("size of float pointer: %lu byte(s)\n", sizeof(float *));
-	printf("size of double pointer: %lu byte(s)\n", sizeof(double *));
 	return (0);
 }
