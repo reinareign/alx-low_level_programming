6eb11a16a9a3beccdce422513f10f1ad4d42f4ba
reinareign <chisohmenem@gmail.com>
2023-10-09 19:52:29 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry Point
- * Return: Always 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n", stdout);
 	return (1);
 }
