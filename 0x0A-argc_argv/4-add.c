f933b9f627673a44d521e641f7085096e4b3ef89
reinareign <chisohmenem@gmail.com>
2023-11-01 19:45:53 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,16 +12,16 @@ int check_num(char *str)
 {
 /*Declaring variables*/
 	unsigned int count;
-	
+
 	count = 0;
 	while (count < strlen(str)) /*count string*/
-	
+
 	{
 		if (!isdigit(str[count])) /*check if str there are digit*/
 		{
 			return (0);
 		}
-		
+
 		count++;
 	}
 	return (1);
@@ -41,17 +41,17 @@ int main(int argc, char *argv[])
 	int count;
 	int str_to_int;
 	int sum = 0;
-	
+
 	count = 1;
 	while (count < argc) /*Goes through the whole array*/
 	{
 		if (check_num(argv[count]))
-		
+
 		{
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
 		}
-		
+
 		/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
@@ -60,9 +60,9 @@ int main(int argc, char *argv[])
 		}
 		count++;
 		}
-	
+
 	printf("%d\n", sum); /*print sum*/
-	
+
 	return (0);
 }
 
