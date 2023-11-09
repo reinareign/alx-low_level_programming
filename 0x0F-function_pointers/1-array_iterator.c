9e2e68d08949eebc142e2baa491341cc97cec98d
reinareign <chisohmenem@gmail.com>
2023-11-09 13:35:19 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 		if (array == NULL || action == NULL)
 			return;
 
-	for (i = 0; d < size; d++)
+	for (d = 0; d < size; d++)
 	{
 		action(array[d]);
 	}
