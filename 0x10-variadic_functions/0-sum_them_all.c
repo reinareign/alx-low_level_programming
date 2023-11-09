b5334f97627ceda9d86d59a1e8b0a6b68b0cf0dc
reinareign <chisohmenem@gmail.com>
2023-11-09 07:55:11 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,16 @@
  * Otherwise - sum of all parameters.
  */
 
-int sum_them_all(const unsigned int n, ...){
+int sum_them_all(const unsigned int n, ...)
+{
 	va_list ap;
 	unsigned int i, sum = 0;
-	
+
 	va_start(ap, n);
-	
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
-	
+
 	va_end(ap);
-	return (sum);}
+	return (sum);
+}
