4eb3fd0eb57980520a0de46cf3e29cd95a7ff2e6
reinareign <chisohmenem@gmail.com>
2023-11-09 18:15:22 -0800

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,6 @@ void print_all(const char * const format, ...)
 {
 	int i = 0;
 	char *str, *sep = "";
-
 	va_list list;
 
 	va_start(list, format);
@@ -44,8 +43,7 @@ void print_all(const char * const format, ...)
 			sep = ", ";
 			i++;
 		}
-	}	
-
+	}
 	printf("\n");
 	va_end(list);
 }
