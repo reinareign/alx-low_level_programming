a8db42c5e9d77bf9ac62aff2f3674bfaca330ba8
reinareign <chisohmenem@gmail.com>
2023-11-27 16:14:05 -0800

    file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and prints the letters.
+ * @filename: the filename.
+ * @letters: the numbers of letters printed.
+ *
+ * Return: the numbers of letters printed. Return 0 if fail.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
